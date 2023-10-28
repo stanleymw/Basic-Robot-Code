1990ee288800d125475ca399ca100ef1708ce71c
stanleymw <107821509+stanleymw@users.noreply.github.com>
2023-10-28 16:02:58 -0700

    change param name

--- src/main/java/frc/robot/commands/TestArmCommand.java
+++ src/main/java/frc/robot/commands/TestArmCommand.java
@@ -11,43 +11,43 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
  * An example command that uses an example subsystem.
  */
 public class TestArmCommand extends CommandBase {
-	private final ArmSubsystem armSubsystem;
+  private final ArmSubsystem armSubsystem;
   private int ticks;
 
-	/**
-	 * Creates a new TestArmCommand.
-	 *
-	 * @param a The ArmSubsystem used by this command.
-	 */
-	public TestArmCommand(ArmSubsystem a) {
-		this.armSubsystem = a;
-		// Use addRequirements() here to declare subsystem dependencies.
-		addRequirements(a);
-	}
+  /**
+   * Creates a new TestArmCommand.
+   *
+   * @param a The ArmSubsystem used by this command.
+   */
+  public TestArmCommand(ArmSubsystem armSubsystem) {
+    this.armSubsystem = armSubsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(armSubsystem);
+  }
 
-	// Called when the command is initially scheduled.
-	@Override
-	public void initialize() {
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
     System.out.println("begin arm test command")
     ticks = 0;
   }
 
-	// Called every time the scheduler runs while the command is scheduled.
-	@Override
-	public void execute() {
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
     ticks++;
     this.armSubsystem.setBothArmControllerSpeed(Math.sin(ticks), Math.cos(ticks));
-	}
+  }
 
-	// Called once the command ends or is interrupted.
-	@Override
-	public void end(boolean interrupted) {
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
     this.armSubsystem.stopAllMotorSpeed();
   }
 
-	// Returns true when the command should end.
-	@Override
-	public boolean isFinished() {
-		return (ticks > 600);
-	}
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return (ticks > 600);
+  }
 }
