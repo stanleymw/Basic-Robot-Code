885b8f6cfb01c37e1a1942591225efd8ca090e43
stanleymw <107821509+stanleymw@users.noreply.github.com>
2023-11-08 19:00:40 -0800

    hw3: arm reset

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.TestArmCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -21,6 +22,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
 	// The robot's subsystems and commands are defined here...
 	private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+	private final ArmSubsystem m_armSubsystem = new ArmSubsystem();
 
 	// Replace with CommandPS4Controller or CommandJoystick if needed
 	private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
@@ -41,13 +44,11 @@ public class RobotContainer {
 	 * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flightjoysticks}.
 	 */
 	private void configureBindings() {
-		// Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-		new Trigger(m_exampleSubsystem::exampleCondition)
-				.onTrue(new ExampleCommand(m_exampleSubsystem));
+		Command resetCommand = Commands.runOnce(() -> {
+			m_armSubsystem.stopAllMotorSpeed();
+		}, ArmSubsystem).andThen(new TestArmCommand());
 
-		// Schedule `exampleMethodCommand` when the Xbox controller's B button is
-		// pressed, cancelling on release.
-		m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    resetCommand.schedule();
 	}
 
 	/**
